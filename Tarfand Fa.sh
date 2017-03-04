848bec8a32256d3e1210ae962b5a59cbdcdd3b92
tgserver2018 <nela@xing886.uu.gl>
2017-03-04 10:40:39 +0330

    Update and rename permag.sh to Tarfand Fa.sh

---
+++ Tarfand Fa.sh
@@ -0,0 +1,58 @@
+#!/usr/bin/env bash
+
+cd $HOME/permag.ir
+
+install() {
+	    cd tg
+		https://t.me/SaMaN_SaNstar2017
+
+sudo pip3 install redis
+
+sudo service redis-server restart
+
+sudo add-apt-repository ppa:ubuntu-toolchain-r/test
+
+sudo apt-get update
+
+sudo apt-get upgrade
+
+sudo apt-get dist-upgrade
+
+
+sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev make unzip git redis-server autoconf g++ libjansson-dev libpython-dev expat libexpat1-dev ppa-purge python3-pip python3-dev
+
+cd $HOME
+
+git clone https://github.com/permag-ir/permag.ir.git
+
+cd permag.ir
+
+ chmod +x permag.sh
+
+./permag.sh install
+
+./Tarfand Fa.sh
+
++98......
+
+---------------------------
+
+@userinfobot
+}
+
+if [ "$1" = "install" ]; then
+  install
+  else
+
+if [ ! -f ./tg/tgcli ]; then
+    echo "tg not found"
+    echo "Run $0 install"
+    exit 1
+fi
+   echo -e "\033[38;5;208m"
+   echo -e "     > best anti spam source SaMaN_SaNstaR"
+   echo -e "                                              \033[0;00m"
+   echo -e "\e[36m"
+   ./tg/tgcli -s ./bot/bot.lua $@
+fi
+
